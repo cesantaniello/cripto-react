23c4e3f0b095f88aaa01de806af83bcab1582776
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2023-09-06 18:03:35 +0200

    Add styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
+import styled from '@emotion/styled'
+
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #FFF
+`
 
 function App() {
 
   return (
     <>
-      <h1>Hola desde App.jsx</h1>
+      <Heading>Hola desde App.jsx</Heading>
     </>
   )
 }
