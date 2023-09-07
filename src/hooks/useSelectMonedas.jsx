3daa42dfd6acb7fac7dee72042ada3c27526d692
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2023-09-07 10:26:08 +0200

    Create Custom hook

---
+++ src/hooks/useSelectMonedas.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const useSelectMonedas = () => {
+
+    const SelectMonedas = () => {
+        console.log('Desde select monedas')
+    }
+
+    return [SelectMonedas]
+
+}
+
+export default useSelectMonedas
